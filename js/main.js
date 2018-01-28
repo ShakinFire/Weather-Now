34e4cbc536574d562179312e87201d734a06b75f
DESKTOP-96GBBGF\aruse <a.rusev1992>
2018-01-29 01:33:14 +0200

    adding main.js file and adding the functionality to the index page.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,168 @@
-$(document).ready(function () {
-    (function boot() {
+var database = (function () {
+    var citiesList = [];
 
-        this.loadQuiz = function () {
-            $('.panel_one h1').show('drop', 500, function() {
-                $('.start_quiz').addClass('started', 500);
-            });
-
-            $('.start_quiz').on('click', function() {
-                showPanel(1);
-                console.log('clicked')
-            })
-        }
-
-        this.showPanel = function(position) {
-            // console.log('show')
-            var current = $('div[data-panel="' + (position - 1) + '"]');
-            var next = $('div[data-panel="' + position + '"]');
-
-            current.find('.wrapper').animate({
-                left: "-=100px",
-                opacity:0
-            }, 500, function() {
-                 current.addClass('hidden');
-                 next.removeClass('hidden');
-                 showNext(next);
-            })
-        };
-
-        this.showNext = function(nextElement) {
-            var wrapper = nextElement.find('.wrapper');
-            wrapper.fadeIn('500', function(){
-
-            })
-
-        }
-
-        loadQuiz();
+    var _addInCitiesList = function (data) {
+        $(data["values"]).each(function (index, elem) {
+            citiesList.push(elem);
+        });
+    }
+    var _getAllCities = (function () {
+        $.getJSON("cities.json", _addInCitiesList);
     })();
 
+    var _flattenObject = function (ob) {
+        var toReturn = {};
+
+        for (var i in ob) {
+            if (!ob.hasOwnProperty(i)) continue;
+
+            if ((typeof ob[i]) == 'object') {
+                var flatObject = _flattenObject(ob[i]);
+                for (var x in flatObject) {
+                    if (!flatObject.hasOwnProperty(x)) continue;
+
+                    toReturn[x] = flatObject[x];
+                }
+            } else {
+                toReturn[i] = ob[i];
+            }
+        }
+        return toReturn;
+    };
+
+    var getWeatherInfo = function (city, unit, successCallback, failCallback) {
+        var un = unit || "metric";
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city +
+            "&appid=b566e35c1181791b83b9aefcbe9be910&units=" + un;
+
+        function makeRequest() {
+            var promise = new Promise(function (resolve, reject) {
+                $.ajax({
+                    url: url,
+                    datatype: "application/json",
+                    success: resolve,
+                    error: reject
+                });
+            })
+            return promise;
+        }
+        // makeRequest().done(successCallback(data));
+        // makeRequest().fail(failCallback());
+        makeRequest().then(function (data) {
+            console.log(data)
+            data = _flattenObject(data);
+            successCallback(data);
+            console.log(data)
+
+        });
+
+        makeRequest().catch(function () {
+            failCallback();
+
+        });
+    }
+
+    return {
+        citiesList,
+        getWeatherInfo
+    }
+
+})();
+
+var DOM = (function () {
+
+    var displayData = function (obj) {
+        // if ($(".content").hasClass('hidden')) {
+        //     $(".content").removeClass('hidden');
+        //     $(".error").remove();
+        // }
+        function _convertFromUnixTimeStamp(t) {
+            var dt = new Date(t * 1000);
+            var hr = dt.getHours();
+            var m = "0" + dt.getMinutes();
+            var s = "0" + dt.getSeconds();
+            return hr + ':' + m.substr(-2) + ':' + s.substr(-2);
+        }
+        $('.city-name').text(obj.name);
+        $('.country-name').text(obj.country);
+        $('.main-description').text(obj.main);
+        $('.temp').text(Math.round(obj.temp));
+        $('.description').text(obj.description);
+
+        $('.temp_max').text(obj.temp_max);
+        $('.temp_min').text(obj.temp_min);
+        $('.sunrise').text(_convertFromUnixTimeStamp(obj.sunrise));
+        $('.sunset').text(_convertFromUnixTimeStamp(obj.sunset));
+        $('.speed').text(obj.speed);
+        $('.clouds').text(obj.all);
+        $('.pressure').text(obj.pressure);
+        $('.humidity').text(obj.humidity);
+    };
+
+    var displayError = function () {
+        alert("no results find");
+    }
+
+    return {
+        displayData,
+        displayError
+    }
+})();
+
+var app = (function () {
+    var unit = "metric";
+
+    var update = function (city, unit) {
+        database.getWeatherInfo(city, unit, DOM.displayData, DOM.displayError);
+
+    };
+
+    var bindEvents = function () {
+        $(".button").on('click', function (e) {
+            e.preventDefault();
+            var value = $(".search").val();
+            update(value, unit)
+        });
+
+        $(".search").autocomplete({
+            source: function (request, response) {
+                var matches = $.map(database.citiesList, function (acItem) {
+                    if (acItem.toUpperCase().indexOf(request.term.toUpperCase()) ===
+                        0) {
+                        return acItem;
+                    }
+                });
+                response(matches);
+            },
+            select: function (event, ui) {
+                update(ui.item.value, unit);
+            }
+
+        });
+
+    }
+
+    var setUnit = function (newUnit) {
+        unit = newUnit;
+    }
+
+    var getUnit = function () {
+        return unit;
+    }
+
+    var init = function () {
+        bindEvents();
+
+        update("Sofia", unit)
+    }
+
+    return {
+        init,
+        getUnit,
+        setUnit,
+    }
+})();
+
+$(document).ready(function () {
+    app.init();
 });
